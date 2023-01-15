378f5ec0e31d0f4b9ff24cdbb2104fae0a594e85
MaulidaRhma <lyellyy@gmail.com>
2023-01-15 09:19:48 +0800

    Merapikan footer, nav, dan sidebar

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,6 @@
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 0.1.0
     </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+    <strong>Copyright &copy; 2022 <a href="#">Praktikum Web</a>.</strong> All rights reserved.
+</footer>
