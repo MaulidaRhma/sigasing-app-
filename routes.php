b7577a5557a895ea5051ec4fe7d2aa28bc6c9a11
MaulidaRhma <lyellyy@gmail.com>
2023-01-18 19:45:11 +0800

    Membuat Perintah Hapus Data

--- routes.php
+++ routes.php
@@ -11,6 +11,9 @@ if (isset($_GET['page'])) {
             break;
         case 'lokasiupdate':
             file_exists('pages/admin/lokasiupdate.php') ? include 'pages/admin/lokasiupdate.php' : include "pages/404.php";
+            break;
+        case 'lokasidelete':
+            file_exists('pages/admin/lokasidelete.php') ? include 'pages/admin/lokasidelete.php' : include "pages/404.php";
             break;    
         default:
             include "pages/404.php";
